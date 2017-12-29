5d4a2fb20bf2cba7f7d609141a77a66c0031a3f9
Anthony S. Gamble <agamble25@gmail.com>
2017-12-29 11:19:46 -0600

    'hello'

---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+	root  "posts#index"
+
+  resources :posts
+  # For details on the D<td><%= post.body %></td>SL available within this file, see http://guides.rubyonrails.org/routing.html
+end

bdcd050d687cac39b1542750db39034f6cefcba4
Anthony S. Gamble <agamble25@gmail.com>
2018-01-09 09:57:09 -0600

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-	root  "posts#index"
+	
+	root "posts#index"
 
   resources :posts
   # For details on the D<td><%= post.body %></td>SL available within this file, see http://guides.rubyonrails.org/routing.html
